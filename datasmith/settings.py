d9cb4b7509fafa320075894962beca126a06628a
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2025-05-23 10:33:23 -0500

    update db settings

--- datasmith/settings.py
+++ datasmith/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'datasmith.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DATABASE", ""),
-        'USER': os.environ.get("POSTGRES_USER", ""),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", ""),
-        'HOST': os.environ.get("POSTGRES_HOST", ""),
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("POSTGRES_DATABASE", ""),
+#         'USER': os.environ.get("POSTGRES_USER", ""),
+#         'PASSWORD': os.environ.get("POSTGRES_PASSWORD", ""),
+#         'HOST': os.environ.get("POSTGRES_HOST", ""),
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
